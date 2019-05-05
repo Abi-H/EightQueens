f2c4fca98d9c1e422bfdc016ba6b68235daf0c65
abi <abiga@Galahad>
2019-05-05 17:16:52 +1200

    Add project files.

---
+++ EightQueens/EightQueen.cs
@@ -0,0 +1,82 @@
+﻿/*****************************************
+*   Solution to the Eight Queens problem *
+*   by Abi Harper                        *
+****************************************/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightQueens
+{
+    class EightQueen
+    {
+        public int Size { get; set; }
+        public EightQueen(int size)
+        {
+            this.Size = size;
+        }
+        public void GetSolution()
+        {
+            int line = 0, pos = -1, count = 0;
+            int[] positions = new int[Size];
+
+            for (int m = 0; m < positions.Length; m++)
+                positions[m] = -1;
+
+            while (true)
+            {
+                for (pos = positions[line] + 1; pos < Size; pos++)
+                {
+                    int i = 0;
+                    for (i = 0; i < line; i++)
+                    {
+                        int dis = line - i;
+                        if (positions[i] == pos || positions[i] == pos + dis || positions[i] == pos - dis)
+                            break;
+                    }
+                    if (i == line)
+                    {
+                        positions[line] = pos;
+                        line++;
+                        if (line == Size)
+                        {
+                            count++;
+                            Console.WriteLine(count);
+                            foreach (int item in positions)
+                            {
+                                for (int j = 0; j < Size; j++)
+                                {
+                                    if (j == item)
+                                        Console.Write("Q ");
+                                    else
+                                        Console.Write("* ");
+                                }
+                                Console.WriteLine();
+                            }
+                            line--;
+                        }
+                        else
+                            break;
+                    }
+                }
+                if (pos == Size)
+                {
+                    if (line == 0)
+                    {
+                        Console.WriteLine("Over");
+                        break;
+                    }
+                    else
+                    {
+                        positions[line] = -1;
+                        line--;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ EightQueens/Program.cs
@@ -0,0 +1,100 @@
+﻿/*****************************************
+*   Solution to the Eight Queens problem *
+*   by Abi Harper                        *
+****************************************/
+
+using System;
+
+namespace EightQueens
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = 8;
+            int[,] solution = new int[8, 8];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    solution[i, j] = 0;
+                }
+            }
+
+            Solve(solution, n);
+            Console.ReadKey();
+        }
+
+        public static void Solve(int[,] solution, int n)
+        {
+            if (PlaceQueens(solution, 0, n))
+            {
+                Console.WriteLine();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(" " + solution[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("No possible solution");
+            }
+        }
+
+        public static bool PlaceQueens(int[,] solution, int queen, int n)
+        {
+            if (queen == n)
+                return true;
+
+            for (int row = 0; row < n; row++)
+            {
+                if (CanPlace(solution, row, queen))
+                {
+                    solution[row, queen] = 1;
+
+                    if (PlaceQueens(solution, queen + 1, n))
+                        return true;
+
+                    solution[row, queen] = 0;
+                }
+            }
+            return false;
+        }
+
+
+
+        public static bool CanPlace(int[,] solution, int row, int col)
+        {
+            int r;
+            int c;
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+
+                    r = row;
+                    c = col;
+
+                    while (r >= 0 && r <= solution.GetUpperBound(0) && c >= 0 && c <= solution.GetUpperBound(1))
+                    {
+                        if (solution[r, c] == 1)
+                            return false;
+
+                        r += y;
+                        c += x;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
+
+
